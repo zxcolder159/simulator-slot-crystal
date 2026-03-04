1042a3b960922c38ab83428e0e047c38fc8ea7ef
zxcolder <qwezitrakspsibkade@gmail.com>
2026-03-04 18:35:46 +0700

    add: user service class

---
+++ src/main/java/simulator/crystal/slot/user/UserService.java
@@ -0,0 +1,35 @@
+package simulator.crystal.slot.user;
+
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Getter
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    /**
+     * Сохраняет нового пользователя в БД.
+     * @param user
+     * @return всегда возвращает пользователя.
+     */
+    @Transactional
+    public User createUser(User user) {
+        if (user.getId() != null && userRepository.existsById(user.getId())) {
+            return userRepository.getReferenceById(user.getId());
+        }
+        return userRepository.save(user);
+    }
+    @Transactional
+    public void addMoney(Long amount, Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        user.setBalance(user.getBalance() + amount);
+        userRepository.save(user);
+    }
+}
