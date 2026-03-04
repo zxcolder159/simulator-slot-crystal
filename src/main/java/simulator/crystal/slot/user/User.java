4c05c95a3ef3b9c5c3ea25b10c12dc1273d7cdb9
zxcolder <qwezitrakspsibkade@gmail.com>
2026-03-04 15:59:45 +0700

    add: user class

---
+++ src/main/java/simulator/crystal/slot/user/User.java
@@ -0,0 +1,20 @@
+package simulator.crystal.slot.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private Long balance;
+    private String avatarUrl = "default-avatar.png";
+}
