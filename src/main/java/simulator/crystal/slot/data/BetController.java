b506a34b25a2518c12f3d0c8560cfee7c162d7fc
zxcolder <qwezitrakspsibkade@gmail.com>
2026-03-18 10:11:57 +0700

    add: bet controller

--- src/main/java/simulator/crystal/slot/data/BetController.java
+++ src/main/java/simulator/crystal/slot/data/BetController.java
@@ -1,4 +1,20 @@
 package simulator.crystal.slot.data;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Getter
+@RequestMapping("/api/v1/history")
 public class BetController {
+	private final BetHistoryRepository betHistoryRepository;
+
+	@GetMapping("/{usedId}")
+	public List<BetHistory> getAllBetHistory(@PathVariable Long userId) {
+		return betHistoryRepository.findAllByUserIdOrderByTimeDesc(userId);
+	}
 }
